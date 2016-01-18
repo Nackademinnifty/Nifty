a22df89810b4d213f311f66e8d813e2ab009e7f9
mihini <manina103@hotmail.com>
2016-01-18 12:44:27 +0100

    new form in contact

--- incl/contact.php
+++ incl/contact.php
@@ -6,5 +6,14 @@ $pageId = 'contact';
 
 <?php include 'incl/header.php'?>
 
+<form>
+	Name:<br>
+	<input type="text" name="name"><br>
+	Email:<br>
+	<input type="email" name="mail"><br>
+	<textarea name="textarea" rows= "10" cols="40">Write your message here</textarea>
+	<input type="submit" value="Send">
+</form>
+
 
 <?php include 'incl/footer.php'?>
