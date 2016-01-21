4d2d11adb80ed55b8a3052e37843ee40c3932564
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-21 20:16:24 +0100

    Major changes to frontpage

--- index.php
+++ index.php
@@ -6,15 +6,17 @@ $pageId = 'home';
 
 <?php include 'incl/header.php'?>
 
-<section id="journey" class="clear">
+<section id="journey">
   <section>
     <h2>Our journey</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
+  <section class="read">
     <a href="about.php">Read more</a>
+  </section>
 </section>
 
-<section id="team" class="clear">
+<section id="team">
   <section class="pictures">
     <img src="img/avatar-1.png">
     <img src="img/avatar-6.png">
@@ -27,7 +29,7 @@ $pageId = 'home';
   </section>
 </section>
 
-<section id="portfolio" class="clear">
+<section id="portfolio">
   <section>
     <h2>Our works</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
