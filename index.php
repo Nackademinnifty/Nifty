bcaacb58a886c1407dcdb8d9bcf022102cb433aa
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-21 13:55:04 +0100

    Made some changes to frontpage

--- index.php
+++ index.php
@@ -6,31 +6,32 @@ $pageId = 'home';
 
 <?php include 'incl/header.php'?>
 
-<section class="journey clear">
-  <h2>Our journey</h2>
+<section class="journey">
   <section>
+    <h2>Our journey</h2>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
+  </section>
+  <a href="about.php">Read more</a>
+</section>
+
+<section class="team">
+  <section>
+    <h2>Our team</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
 </section>
 
-<section class="team clear">
-  <h2>Our team</h2>
+<section class="portfolio">
   <section>
+    <h2>Our works</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
 </section>
 
-<section class="portfolio clear">
-  <h2>Our works</h2>
+<section class="contact">
   <section>
+    <h2>Nifty Webproductions</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
-  </section>
-</section>
-
-<section class="contact clear">
-  <h2>Nifty Webproductions</h2>
-  <section>
-  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
 </section>
 
