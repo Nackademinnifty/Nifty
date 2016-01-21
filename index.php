5cfb331fb8b993bc2f7ad7a5e73742a94065699d
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-21 19:01:38 +0100

    Removed section

--- index.php
+++ index.php
@@ -34,12 +34,5 @@ $pageId = 'home';
   </section>
 </section>
 
-<section id="nifty" class="clear">
-  <section>
-    <h2>Nifty Web</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
-  </section>
-</section>
-
 
 <?php include 'incl/footer.php'?>
