02e2444f84946120f5438dec0ea70317f4e99304
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-22 14:02:04 +0100

    Made som final changes

--- index.php
+++ index.php
@@ -7,17 +7,17 @@ $pageId = 'home';
 <?php include 'incl/header.php'?>
 
 <section class="section" id="journey">
-  <section>
+  <section class="section">
     <h2>Our journey</h2>
     <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
-  <section class="read">
+  <section class="read section">
     <a href="about.php">Read more</a>
   </section>
 </section>
 
 <section class="section" id="team">
-  <section class="pictures">
+  <section class="pictures section">
     <img src="img/avatar-1.png">
     <img src="img/avatar-6.png">
     <img src="img/avatar-1.png">
@@ -31,7 +31,7 @@ $pageId = 'home';
 </section>
 
 <section class="section" id="portfolio">
-  <section>
+  <section class="section">
     <h2>Our works</h2>
     <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
