92f463d6129d85ce32e7bc7f351ce8157c0baa21
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-22 10:35:10 +0100

    Commit to solve conflict

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ $pageId = 'home';
 <section id="journey">
   <section>
     <h2>Our journey</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
   <section class="read">
     <a href="about.php">Read more</a>
@@ -25,15 +25,17 @@ $pageId = 'home';
   </section>
   <section>
     <h2>Our team</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
+  <a href="about.php">Meet us!</a>
 </section>
 
 <section id="portfolio">
   <section>
     <h2>Our works</h2>
-    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
+    <p class="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sapien arcu, iaculis a sem sit amet, porta dignissim urna. Aliquam non felis mattis, finibus nisi vitae, lacinia erat. Cras vitae placerat ex. Aliquam erat volutpat. Curabitur quis convallis odio. Sed id erat tristique, imperdiet eros in, imperdiet massa. Aliquam ut imperdiet dolor. Fusce efficitur dui quis ullamcorper molestie. Nullam facilisis rhoncus lectus id varius. Nulla lacus ex, hendrerit ut nunc non, porta ullamcorper nisi. Quisque gravida mi eu neque blandit porta. Proin ut pulvinar purus, in rutrum arcu.</p>
   </section>
+  <a href="work.php">View our work</a>
 </section>
 
 
