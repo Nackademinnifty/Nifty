a4fa1ae49a099224d384df97905c35854eba56fc
Victor Trigo Wagner <Victor Trigo Wagner>
2016-01-22 15:07:58 +0100

    about-sida med css

--- about.php
+++ about.php
@@ -4,41 +4,73 @@ $pageId = 'about';
 ?>
 
 
-<?php include 'incl/header.php'?>
-
-<section>
-	<h2>We work here</h2>
-	<p>Some text about us</p>
-	
-	
-	<section>
-		<h3>Robin</h3>
-		<p>Text om Robin</p>
-		<img src="#">
-		
-	</section>
-	
-	<section>
-		<h3>Joel</h3>
-		<p>Text om Joel</p>
-		<img src="#">
-	</section>
-	
-	<section>
-		<h3>Victor</h3>
-		<p>Text om Victor</p>
-		<img src="#">
-	</section>
-	
-	<section>
-		<h3>Nina</h3>
-		<p>Text om Nina</p>
-		<img src="#">
-	</section>
-
-</section>
-	
+	<?php include 'incl/header.php'?>
+		<h1 id="foo"></h1>
 
 
+		<div class="aboutIntro">
+			<h2>We work here!</h2>
+			<p>We're a small company of four hightly driven och skilled web-developers</p>
+		</section>
 
-<?php include 'incl/footer.php'?>
+		<section class="employeeWrap clear">
+			<div class="employeeImage" id="victor"></div>
+			<div class="employeeDesc">
+				<div class="employeeName">
+					<h3>Name</h3>
+					<h2>Victor Trigo Wagner</h2><hr /></div>
+				<div class="employeeProfile">
+				</div>
+				<div class="employeeTitle">
+					<h3>Title</h3>
+					<h2>Front-end developer</h2><hr />
+				</div>
+				<div class="employeeQuote clear">
+					<h3>Quote</h3>
+					<h4>"Hello, I'm Dr. Bean. Apparently. And my job is to sit and look at paintings. So, what have I learned that I can say about this painting? Well, firstly, it's really quite big, which is excellent. because If it were really small, you know, microscopic, then hardly anyone would be able to see it."</h4></div>
+			</div>
+		</section>
+
+		<section class="employeeWrap clear">
+			<div class="employeeImage" id="robin"></div>
+			<div class="employeeDesc">
+				<div class="employeeName">
+					<h3>Name</h3>
+					<h2>Christopher Walken</h2><hr /></div>
+				<div class="employeeProfile">
+				</div>
+				<div class="employeeTitle">
+					<h3>Title</h3>
+					<h2>Front-end developer</h2><hr />
+				</div>
+				<div class="employeeQuote clear">
+					<h3>Quote</h3>
+					<h4>"I don't know why people eat so badly. I could eat pasta all the time, but it really is fattening. And I love ice cream, but I can't do that. There was a time, until I was in my mid-forties, when I could eat a whole pizza - and really, no effect."</h4></div>
+			</div>
+		</section>
+
+
+		<div class="aboutIntro">
+			<h2>What we offer!</h2>
+			<p>Highly customizable, responsive, mobile-friendly websites that help your business grow!</p>
+		</section>
+
+		<div class="aboutIntro">
+			<h2>What we have won!</h2>
+			<p>With hard work and determination we have been awarded theese fine prizes!</p>
+		</section>
+
+		<div class="aboutIntro">
+			<h2>What we belive in!</h2>
+			<p>With great customers comes great responsibility!</p>
+		</section>
+
+		<div class="aboutIntro">
+			<h2>Where we come from!</h2>
+			<p>Four highly motivated students who decided bring true beauty to the web!</p>
+		</section>
+
+
+
+
+		<?php include 'incl/footer.php'?>
