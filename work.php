dec1ebad9a1e1ce70901bacdf546355497b1a7aa
Nackademinnifty <nackademin.nifty@gmail.com>
2016-01-22 10:31:58 +0100

    started with wok.php, changed section to .section in start.scss

--- work.php
+++ work.php
@@ -4,50 +4,46 @@ $pageId = 'works';
 ?>
 
 
-<?php include 'incl/header.php'?>
+    <?php include 'incl/header.php'?>
 
-<section>
-Client: Craftfood
-Project: Website
-Link: <a href="#"></a>
-Description: Craftfood is a restaurant that started 2015 in Stockholm. They needed a new webpage with information about their food and philosophy. 
-The webpage is responsive and works on every screen size even on mobiles. You can even make reservations on the page.
-</section>
+        <section class="work">
 
-<section>
-Client: Z
-Project: Tittut
-Link: <a href="#">Länk</a>
-Description: Z is one of the largest retail companies in Sweden. They wanted us to develop a campaign solution for their new product Tittut and wanted us to make the campaign page simple and attractive. We made a simple website that is easy to navigate.
-</section>
-
-<section>
-Client: Training 365 
-Project: Website and app
-Link: <a href="#">Training 365</a>
-Description:  Traning 365 wanted us to make a site and app for their members so that are easier for them to book group training. The site will also give inspirational and motivational articles for the members to workout more. We made apps both for android and iOS. The website is also responsive which means that the members doesn’t need to download the app to surf the site.
-</section>
-
-<section>
-Client: Y 
-Project: Responsive website
-Link: <a href="#">Länk</a>
-Description: Y is a post-graduate school that needed a new website. Their old site needs to be freshening up a bit and make it responsive. We made the new site colorful to attract more young people.
-</section>
-
-<section>
-Client: Eko
-Project: Website
-Link: <a href="#">Länk</a>
-Description: Eko is a project that wanted Swedish people to be more aware of the environment and be more environments friendly. We developed a website so promote the project and to share more information. 
-</section>
-
-<section>
-Client: Q
-Project: Website
-Link: <a href="#">Länk</a>
-Description: Q is a new started company and already have a simple website. They needed to upgrade their site with a webshop to sell their famous watches. We made a sleek and stylish website to match their profile. Now it’s easier for the customers to buy watches from their site.
-</section>
+            <header class="sectionHeader">
+                <h3>Our work</h3>
+                <p>At nifty we take great pride in our work.</p>
+                <p>Take a look further down to see some selected work that we are extra proud of.</p>
+            </header>
 
 
-<?php include 'incl/footer.php'?>
+            <article id="craftfood">
+                <h2>Project: Craftfood</h2>
+                <p>Craftfood is a restaurant that started 2015 in Stockholm...</p>
+                <a href="craftfood">Read More</a>
+            </article>
+
+            <article id="tittut">
+
+                <h2>Tittut</h2>
+                <p>Z is one of the largest retail companies in Sweden...</p>
+                <a href="tittut.php">Read More</a>
+
+            </article>
+
+            <article id="training365">
+                <h2>Traning 365:</h2>
+                <p>Traning 365 wanted us to make a site and app for their members..</p>
+                <a href="training365.php">Training 365</a>
+            </article>
+
+        </section>
+
+        <article id="eko">
+            <h2>Eko:</h2>
+            <p>Eko is a project that wanted Swedish people to be more aware of...</p>
+            <a href="eko.php">Read More</a>
+        </article>
+
+
+
+
+        <?php include 'incl/footer.php'?>
