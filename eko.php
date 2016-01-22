4d8291e36c0738863c824e9b69efce62c2af41b9
Nackademinnifty <nackademin.nifty@gmail.com>
2016-01-22 14:50:28 +0100

    färdig med de enskilda rprojektsidona för mobil

---
+++ eko.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitle = 'Eko';
+$pageId = 'Eko';
+?>
+
+
+    <?php include 'incl/header.php'?>
+
+        <section class="project">
+            <article>
+                <h2>Client: <span>Eko Project</span></h2>
+                <h2>Project: <span>New website</span></h2>
+                <h2>Link:<a href="#">eko.se</a></h2> 
+                <h2>Description:</h2>
+                <p>Eko is a project that wanted Swedish people to be more aware of the environment and be more environments friendly. We developed a website so promote the project and to share more information.</p>
+                
+                <figure>
+                    <a target="_top" href="img/work/eko.jpg"><img src="img/work/eko.jpg" alt="eko, website"></a>
+                </figure>
+            </article>
+        </section>
+
+
+        <?php include 'incl/footer.php'?>
---
+++ tittut.php
@@ -0,0 +1,24 @@
+<?php
+$pageTitle = 'Tittut';
+$pageId = 'Tittut';
+?>
+
+
+    <?php include 'incl/header.php'?>
+
+        <section class="project">
+            <article>
+                <h2>Client: <span>Z</span></h2>
+                <h2>Project: <span>Tittut Webpage</span></h2>
+                <h2>Link:<a href="#">tittut.io</a></h2> 
+                <h2>Description:</h2>
+                <p>Z is one of the largest retail companies in Sweden. They wanted us to develop a campaign solution for their new product Tittut and wanted us to make the campaign page simple and attractive. We made a simple website that is easy to navigate.</p>
+                
+                <figure>
+                    <a target="_top" href="img/work/tittut.jpg"><img src="img/work/tittut.jpg" alt="training365 page"></a>
+                </figure>
+            </article>
+        </section>
+
+
+        <?php include 'incl/footer.php'?>
