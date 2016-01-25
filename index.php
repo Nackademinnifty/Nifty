42d5d25587a287cabe3371f9706aed92ad1dbb33
Robin Zetterström <robin.zetterstrom@yh.nackademin.se>
2016-01-25 13:18:39 +0100

    Fixed som classes in _start.scss

--- index.php
+++ index.php
@@ -7,47 +7,47 @@ $pageId = 'home';
 <?php include 'incl/header.php'?>
 
 <!-- Company History -->
-<div class="section" id="journey">
-  <section class="section">
+<section class="viewportSection" id="journey">
+  <section class="column">
     <h2>Our journey</h2>
-    <p class="text">
+    <p class="paragraph">
       Since our founding in late 2015, our client list has already included a broad range of companies. We believe our achivements are proof of our commitment to delivering outstanding results for our clients. Our whole company is built on a great portion of passion. Simply talk to any member of our team and you will quickly sense that something is different at Nifty.
     </p>
   </section>
-  <div class="read section">
+  <section class="column">
     <a href="about.php#offerWrap">Read more</a>
-  </div> 
-</div> <!-- /END History -->
+  </section> 
+</section> <!-- /END History -->
 
 <!-- Employees -->
-<div class="section" id="team">
-  <div class="pictures section">
+<section class="viewportSection" id="team">
+  <figure class="pictures column">
     <a href="about.php#joel"><img src="img/joel.jpg" alt="Joel Sjöström" /></a>
     <a href="about.php#robin"><img src="img/robin.jpeg" alt="Robin Zetterström"></a>
     <a href="about.php#nina"><img src="img/nina.jpg" alt="Nina Ma"></a>
     <a href="about.php#victor"><img src="img/victor.jpg" alt="Victor T Wagner"></a>
-  </div>
-  <section class="section">
+  </figure>
+  <section class="column">
     <h2>Our team</h2>
-    <p class="text">
+    <p class="paragraph">
       Meet our incredible team that make us one of the leading web development companies in the industry.
     </p>
   </section>
   <a class="btnCtrl" href="about.php">Meet our team!</a>
-</div> <!-- /END Employees -->
+</section> <!-- /END Employees -->
 
 <!-- Our showcase -->
-<div class="section" id="portfolio">
-  <section class="section">
+<section class="viewportSection" id="portfolio">
+  <section class="column">
     <h2>Our works</h2>
-    <p class="text">
+    <p class="paragraph">
       <b>We're working hard to impress ourselfs and our customers.</b>
       Everyday we do something new that matters to many and togheter we're building a website, the company's best friend that are always there 24/7 throughout the year and making profit.<br>
       Watch our recent projects and please contact us with your project inquery if you like what you see or we could just be friends!
     </p>
   </section>
   <a href="work.php">View our work</a>
-</div> <!-- /END Showcase -->
+</section> <!-- /END Showcase -->
 
 
 <?php include 'incl/footer.php'?>
