8ddeef4c98f4080b2040f4c478b7b68d45d54e0e
Nackademinnifty <nackademin.nifty@gmail.com>
2016-01-25 10:44:30 +0100

    media queries för logo

---
+++ incl/logo.php
@@ -0,0 +1,43 @@
+<div class="svg">
+<svg version="1.1" id="svg" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+	 viewBox="0 0 804.9 240.9" style="enable-background:new 0 0 804.9 240.9;" xml:space="preserve">
+<polygon class="st0" points="735.4,240.9 665.8,200.7 735.4,160.6 "/>
+<polygon class="st1" points="665.8,120.4 735.4,160.6 665.8,200.7 "/>
+<polygon class="st2" points="735.4,160.6 665.8,120.4 735.4,80.3 "/>
+<polygon class="st3" points="735.4,80.3 804.9,120.4 735.4,160.6 "/>
+<polygon class="st0" points="804.9,120.4 735.4,80.3 804.9,40.1 "/>
+<polygon class="st1" points="665.8,40.1 735.4,80.3 665.8,120.4 "/>
+<polygon class="st2" points="665.8,120.4 596.3,80.3 665.8,40.1 "/>
+<polygon class="st3" points="596.3,0 665.8,40.1 596.3,80.3 "/>
+<polygon class="st0" points="586.3,240.9 516.8,200.7 586.3,160.6 "/>
+<polygon class="st1" points="516.8,120.4 586.3,160.6 516.8,200.7 "/>
+<polygon class="st2" points="586.3,80.3 655.8,120.4 586.3,160.6 "/>
+<polygon class="st3" points="586.3,160.6 516.8,120.4 586.3,80.3 "/>
+<polygon class="st0" points="516.8,40.1 586.3,80.3 516.8,120.4 "/>
+<polygon class="st1" points="516.8,120.4 447.2,80.3 516.8,40.1 "/>
+<polygon class="st2" points="447.2,0 516.8,40.1 447.2,80.3 "/>
+<polygon class="st3" points="367.7,240.9 298.1,200.7 367.7,160.6 "/>
+<polygon class="st0" points="298.1,120.4 367.7,160.6 298.1,200.7 "/>
+<polygon class="st1" points="367.7,160.6 298.1,120.4 367.7,80.3 "/>
+<polygon class="st2" points="367.7,80.3 437.2,120.4 367.7,160.6 "/>
+<polygon class="st3" points="298.1,40.1 367.7,80.3 298.1,120.4 "/>
+<polygon class="st0" points="367.7,80.3 298.1,40.1 367.7,0 "/>
+<polygon class="st1" points="367.7,0 437.2,40.1 367.7,80.3 "/>
+<polygon class="st2" points="288.1,240.9 218.6,200.7 288.1,160.6 "/>
+<polygon class="st3" points="218.6,120.4 288.1,160.6 218.6,200.7 "/>
+<polygon class="st0" points="288.1,160.6 218.6,120.4 288.1,80.3 "/>
+<polygon class="st1" points="218.6,40.1 288.1,80.3 218.6,120.4 "/>
+<polygon class="st2" points="288.1,80.3 218.6,40.1 288.1,0 "/>
+<polygon class="st3" points="208.6,240.9 139.1,200.7 208.6,160.6 "/>
+<polygon class="st0" points="139.1,120.4 208.6,160.6 139.1,200.7 "/>
+<polygon class="st1" points="208.6,160.6 139.1,120.4 208.6,80.3 "/>
+<polygon class="st2" points="139.1,40.1 208.6,80.3 139.1,120.4 "/>
+<polygon class="st3" points="139.1,120.4 69.5,80.3 139.1,40.1 "/>
+<polygon class="st0" points="69.5,0 139.1,40.1 69.5,80.3 "/>
+<polygon class="st1" points="69.5,0 69.5,80.3 0,40.2 "/>
+<polygon class="st2" points="0,40.1 69.5,80.3 0,120.4 "/>
+<polygon class="st3" points="69.5,160.6 0,120.4 69.5,80.3 "/>
+<polygon class="st0" points="0,120.4 69.5,160.6 0,200.7 "/>
+<polygon class="st1" points="69.5,160.6 69.5,240.9 0,200.7 "/>
+</svg>
+</div>
